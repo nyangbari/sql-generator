5e82e4df502053274ea89d3bbb3cad8a849d0e15
Nyangbari <hyojin.ju@gmx.net>
2026-01-13 13:43:22 +0900

    Fix: Add QueryPreprocessor to services __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -2,5 +2,6 @@
 from .rag_service import RAGService
 from .sql_service import SQLService
 from .validation_service import ValidationService
+from .query_preprocessor import QueryPreprocessor
 
-__all__ = ['RAGService', 'SQLService', 'ValidationService']
+__all__ = ['RAGService', 'SQLService', 'ValidationService', 'QueryPreprocessor']
